f5abed0e84c5777b592a7c4342c61f7f971bb928
Sky <dungchogithub@gmail.com>
2018-10-15 15:05:09 -0700

    Longest Palindromic Substring

---
+++ python/Leetcode5_LongestPalindromicSub.py
@@ -0,0 +1,31 @@
+'''
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Input: "cbbd"
+Output: "bb"
+'''
+def PalindromeCheck(str):
+	if len(str) <= 1:
+		return True
+	if str[0] != str[-1]:
+		return False
+	return PalindromeCheck(str[1:-1])
+
+def longestPalindromicSub(str):
+	longest = 0
+	result = ''
+	for i in range(len(str)):
+		count = 1
+		for j in range(i+1, len(str)):
+			if str[i] == str[j]:
+				if PalindromeCheck(str[i:j+1]) and count > longest:
+					result = str[i:j+1]
+					longest = count
+					print(result)
+				break
+			count += 1
+	return result
+
+print(longestPalindromicSub('cbbd'))
