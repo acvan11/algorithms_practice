ae4c9b712d8eee6794e71b3a9fb65ec80fc8e9a0
Sky <dungchogithub@gmail.com>
2018-10-15 13:48:55 -0700

    Longest substring without repeating characters

--- python/Leetcode3_Longest_Substring.py
+++ python/Leetcode3_Longest_Substring.py
@@ -1,16 +1,36 @@
+'''
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
 
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+'''
 def longestSub(s):
-	max = 0
-	seen = ''
-	i = 0
-	while i < len(s):
-		if s[i] not in seen:
-			seen += s[i]
-		else:
-			if len(seen) > max: max = len(seen)
-			seen = seen[seen.index(s[i]) + 1:] + s[i]
-		i += 1
-	if len(seen) > max: max = len(seen)
-	return max
-
-print(longestSub('dabcedabcd'))
+	print(s)
+	longest = 0
+	result = ''
+	for i in range(len(s)):
+		count = 1
+		for j in range(i+1, len(s)):
+			if s[j] in s[i:j]:
+				break
+			count += 1
+		if count > longest:
+			longest = count
+			result = s[i:j]
+	return result
+print('longest sub without repeating: ',longestSub('dabcedabcd'))
+print('=======')
+print(longestSub('bbbb'))
+print('=======')
+print(longestSub('abcabcbb'))
+print('=======')
+print(longestSub('pwwkew'))
