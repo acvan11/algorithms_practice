ee721f500df42b308c8a86c458d84d5b02cb8d42
Sky <dungchogithub@gmail.com>
2018-11-01 16:34:03 -0700

    Heap

--- python/Leetcode18_4Sum.py
+++ python/Leetcode18_4Sum.py
@@ -1,13 +1,27 @@
 def sum4(arr, target):
 	result = []
+	arr.sort()
 	for i in range(len(arr) - 3): 
 		for j in range(i+1, len(arr) -2):
-			for k in range(j+1, len(arr) -1):
-				for l in range(k+1, len(arr)):
-					if arr[i] + arr[j] + arr[k] + arr[l] == target:
-						result2 = [arr[i], arr[j], arr[k], arr[l]]
-						if not checkDuplicate(result, result2):
-							result.append(result2)
+			count = target - arr[i] - arr[j]
+			low = j + 1
+			high = len(arr) -1
+			while low < high:
+				if arr[low] + arr[high] < count:
+					low +=1
+				elif arr[low] + arr[high] > count:
+					high -= 1
+				else:
+					result2 = [arr[i], arr[j], arr[low], arr[high]]
+					print(result2)
+					if not checkDuplicate(result, result2):
+						result.append(result2)
+			# for k in range(j+1, len(arr) -1):
+			# 	for l in range(k+1, len(arr)):
+			# 		if arr[i] + arr[j] + arr[k] + arr[l] == target:
+			# 			result2 = [arr[i], arr[j], arr[k], arr[l]]
+			# 			if not checkDuplicate(result, result2):
+			# 				result.append(result2)
 	print(result)
 
 def checkDuplicate(bigarr, smallarr):
